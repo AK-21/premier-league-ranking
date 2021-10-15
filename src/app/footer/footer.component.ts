64f620bb52db9906b2a5f6913bed3651d991b613
Arkadiusz Kostyra <kostyraarkadiusz@gmail.com>
2021-10-15 23:05:36 +0200

    Implemented simple time displaying system

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { BooleanLiteral } from 'typescript';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
+
 export class FooterComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.time = this.getCurrentDateString();
   }
 
+  time: string; 
+
+  private initTimer(): void {
+
+    const TIME_UPDATE_INTERVAL = 60000;
+
+    setInterval(()=>{
+      this.time = this.getCurrentDateString()
+    }, TIME_UPDATE_INTERVAL);
+  }
+
+  ngOnInit(): void {
+    this.initTimer();
+  }
+
+  getCurrentDateString(): string {
+    let date = new Date();
+    return date.getDate()
+      + '.'
+      + (date.getMonth()+1)
+      + '.'
+      + date.getFullYear()
+      + ' '
+      + date.getHours()
+      + ':'
+      + date.getMinutes()
+  }
 }
